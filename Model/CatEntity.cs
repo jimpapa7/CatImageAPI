731ada154bb667d50fedcd273f82c4c795beb8a1
jimpapa7 <36934982+jimpapa7@users.noreply.github.com>
2024-10-12 11:58:44 +0300

    Update CatEntity.cs

--- Model/CatEntity.cs
+++ Model/CatEntity.cs
@@ -4,13 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 public class CatEntity
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }  // Auto-incremental unique integer for the cat in your database
+    public int Id { get; set; }  // Auto-incremental unique integer for the cat in the database
     [Key]
     public string CatId { get; set; }  // The id returned by the Cat API
     [Required]
     public int Width { get; set; }  // Image width returned by the Cat API
     [Required]
     public int Height { get; set; }  // Image height returned by the Cat API
-    public byte[] Image { get; set; }  // You will decide how to store the image (URL, binary, etc.)
+    public byte[] Image { get; set; }  
     public DateTime Created { get; set; } = DateTime.Now;  // Timestamp of record creation
 }
